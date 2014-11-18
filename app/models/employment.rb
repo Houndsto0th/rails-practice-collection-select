712a6e33a3777890045464bb3f61b597e7e7ebf4
Houndsto0th <bry.kinnear@gmail.com>
2014-11-18 12:01:39 -0700

    added collection select and validations

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,3 +1,13 @@
 class Employment < ActiveRecord::Base
 
+  belongs_to :organization
+  belongs_to :person
+  belongs_to :job_title
+  belongs_to :location
+
+  validates :organization, presence: true
+  validates :person, presence: true
+  validates :job_title, presence: true
+  validates :location, presence: true
+
 end
